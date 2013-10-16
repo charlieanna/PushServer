a55b883b5c226e4be2883c0a7ce9304e843bce3a
Ankur <ankothari@gmail.com>
2013-10-16 20:16:05 +0530

    Pushserver to PushSever

--- Rakefile
+++ Rakefile
@@ -3,4 +3,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-Pushserver::Application.load_tasks
+PushServer::Application.load_tasks
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130408072533) do
+
+  create_table "apps_users", id: false, force: true do |t|
+    t.integer "app_id"
+    t.integer "user_id"
+  end
+
+  create_table "articles_categories", id: false, force: true do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "devices", force: true do |t|
+    t.string   "device_identifier"
+    t.string   "registration_id"
+    t.string   "device_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "messages", force: true do |t|
+    t.string   "to"
+    t.string   "from"
+    t.string   "subject"
+    t.text     "body",       limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "app_id"
+  end
+
+  create_table "messages_users", force: true do |t|
+    t.integer  "message_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "jabber_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "device_identifier"
+  end
 
 end
