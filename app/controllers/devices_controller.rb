65c2c5d0ff4ba690829767045171e62f743b4f3e
ankur kothari <ankothari@gmail.com>
2013-12-02 15:52:06 +0530

    remved the device_params fromd evice_controller

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -3,7 +3,11 @@ class DevicesController < ApplicationController
   def create
     device = Device.find_by_device_identifier(params[:device_identifier])
       if device.nil?
-        device = Device.create(device_params)
+        device = Device.new
+        device.device_identifier =  params[:device_identifier]
+        device.registration_id = params[:registration_id]
+        device.device_type = params[:device_type]
+        device.save
       else
         unless device.registration_id != params[:registration_id]
           device.registration_id = params[:registration_id]
@@ -13,9 +17,7 @@ class DevicesController < ApplicationController
     render text: 'OK'
   end
 
-  private
+  
 
-  def device_params
-    params.require(:device).permit(:device_identifier, :registration_id, :device_type)
-  end
+ 
 end
